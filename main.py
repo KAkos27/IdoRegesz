75c0eba0fd343d93025b0d1297a4ca83c6b1acde
lajkogege <lajkogege@gmail.com>
2023-12-13 14:17:51 +0100

    h

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ import helyszinek
 targyak=["kulcs","penz","asdas"]
 
 helyszinek.kezdes(targyak)
+helyszinek.mezo(targyak)
+helyszinek.kut(targyak)
+helyszinek.penz_felvetele()
+helyszinek.kastely()
 
