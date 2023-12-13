2df49672d29fd28a9aee24f83b38c4ed8af48508
Kakos27 <akoskosztolanyi@gmail.com>
2023-12-13 13:44:40 +0100

    lapkiiras reszponzivva tevese

--- lap.py
+++ lap.py
@@ -1,15 +1,24 @@
 def jelsor(jel,meret):
-    print(jel*meret)
+    for i in range(0,meret,1):
+        print(jel, end="")
 
-def szoveg(jel2,szoveg,meret):
-    hossz: int= meret- (len(jel2) + len(jel2))
+def szovegsor(jel2,szoveg,meret):
+    hossz: int= int(meret) - (len(jel2) + len(jel2))
     print(f"{jel2}{szoveg:^{hossz}}{jel2}")
 
-def fooldal(szoveg,lista,lapmeret):
-    jelsor("-",lapmeret)
-    szoveg("*","asd",lapmeret)
-    jelsor("-",lapmeret)
+def listasor(jel2,lista,meret):
+    hossz: int= int(meret) - (len(jel2) + len(jel2))
     for i in range(0,len(lista),1):
-        szoveg("*","asd",lista[i])
-    jelsor("-",lapmeret)
+        print(f"{jel2}{(lista[i]):^{hossz}}{jel2}")
+
+def fooldal(szoveg,lapmeret,lista,jel,jel2):
+    jelsor(jel,lapmeret)
+    print()
+    szovegsor(jel2,szoveg,lapmeret)
+    print()
+    jelsor(jel,lapmeret)
+    print()
+    listasor(jel2,lista,lapmeret)
+    jelsor(jel,lapmeret)
+    print()
 
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import lap
-targyak:str=[]
-lapmeret=60
+targyak=["kulcs","penz","asdas"]
+szoveg ="Napfényes mezőn állsz, egy kút előtt. Itt van: pénz. Nyugatra egy hatalmas kastélyt látsz."
+lapmeret=len(szoveg)+2
 
-lap.fooldal():
-
+lap.fooldal(szoveg,lapmeret,targyak,"-","*")
 
 
