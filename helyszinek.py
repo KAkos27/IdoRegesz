ab53e4a3c3b71f4d6ce1a96585c2e12b57a4b5df
lajkogege <lajkogege@gmail.com>
2023-12-13 14:12:55 +0100

    tz

--- helyszinek.py
+++ helyszinek.py
@@ -1,6 +1,6 @@
 
 import lap
-
+penz = 0
 def kezdes(targyak):
     szoveg = "Egy óriási mezőn vagy. Nyugat felé egy hatalmas épület körvonalai tűnnek fel."
     lapmeret=len(szoveg)+2
@@ -23,35 +23,40 @@ def mezo(targyak):
     elif masodik_leiras_kicsi == "megy var" or "megy vár":
         kastely()
   
-def kut(): 
-    print("Napfényes mezőn állsz, egy kút előtt. Itt van: pénz. Nyugatra egy hatalmas kastélyt látsz.")
+def kut(penz, targyak): 
+    szoveg = "Napfényes mezőn állsz, egy kút előtt. Itt van: pénz. Nyugatra egy hatalmas kastélyt látsz."
+    lapmeret=len(szoveg)+2
+    lap.fooldal(szoveg,lapmeret,targyak,"-","*")
     felvesz=input("> ")
     felvesz_kicsi=felvesz.lower()
-    harmadik_leiras:str=input("> ")
-    harmadik_leiras_kicsi=harmadik_leiras.lower()
     if felvesz_kicsi == "felvesz penz" or "felvesz pénz":
-        penz_felvetele()
-    if harmadik_leiras_kicsi == "megy epulet" or "megy épület":
+        penz-=1
+        if penz == 0:
+            penz_felvetele()
+        print("Már felvetted a pénzt")
+    if felvesz_kicsi == "megy epulet" or "megy épület":
       kastely() 
-    elif harmadik_leiras_kicsi== "megy kastely" or "megy kastély":
+    elif felvesz_kicsi == "megy kastely" or "megy kastély":
       kastely()
 
 def penz_felvetele():
      print("Rendben, a pénzt elraktad")
-     negyedik_leiras:str=input("> ")
-     negyedik_leiras_kicsi:str=negyedik_leiras.lower()
+     kut(penz)
+     """negyedik_leiras_kicsi:str=negyedik_leiras.lower()
      if negyedik_leiras_kicsi == "megy epulet" or "megy épület" or "megy kastely" or "megy kastély":
-        kastely()
+        kastely()"""
         
 def kastely():
-    print("A várudvaron állsz. Nyugatra nyitott kamrát, északra zárt ajtót látsz. Egy széles lépcső vezet fel a vártemplomhoz")
-    otodik_leiras:str=input("> ")
-    otodik_leiras_kicsi=otodik_leiras.lower()
-    if otodik_leiras_kicsi == "megy vartemplom" or "megy vártemplom":
+    szoveg = "A várudvaron állsz. Nyugatra nyitott kamrát, északra zárt ajtót látsz. Egy széles lépcső vezet fel a vártemplomhoz"
+    lapmeret=len(szoveg)+2
+    lap.fooldal(szoveg,lapmeret,targyak,"-","*")
+    leiras:str=input("> ")
+    eiras_kicsi=otodik_leiras.lower()
+    if leiras_kicsi == "megy vartemplom" or "megy vártemplom":
       print()
-    elif otodik_leiras_kicsi== "megy kamra":
+    elif leiras_kicsi== "megy kamra":
      print()
-    elif otodik_leiras_kicsi == "megy ajto" or "megy ajtó":
+    elif leiras_kicsi == "megy ajto" or "megy ajtó":
         print()
     
       
